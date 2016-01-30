4a8a2dcd02815ee22598051d8a36fdf523645b87
AlexTraher <alex.traher@gmail.com>
2016-01-30 18:59:21 +0000

    Add functionality for usernames

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ io.on('connection', function (socket) {
   socket.on('chat message', function (msg) {
     io.emit('chat message', msg);
   });
+  
+  socket.on('typing', function (name) {
+    io.emit('userTyping', name);
+  })
 
   socket.on('disconnect', function () {
     console.log('user disconnected');
